01d6c4c1138a21d84f5f8b807472821c61406b25
clmnt <huvigclement@gmail.com>
2024-04-26 18:23:32 +0700

    Update Create component UI and add video upload functionality

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView, Text, TouchableOpacity, View, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import { Video, ResizeMode } from "expo-av";
+
+import { useState } from "react";
+import { icons } from "../../constants";
 
 const Create = () => {
-  return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  )
-}
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    thumbnail: null,
+    video: null,
+    prompt: "",
+  });
 
-export default Create
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a catchy title..."
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100">Upload Video</Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-40 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                  <Image
+                    source={icons.upload}
+                    resizeMethod="contain"
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Create;
