428b4d7c56bab9b812b762945279b644156e74f7
clmnt <huvigclement@gmail.com>
2024-04-27 17:49:36 +0700

    Add expo-document-picker package and implement file upload functionality

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,14 +1,19 @@
-import { ScrollView, Text, TouchableOpacity, View, Image } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View, Image, Alert } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Video, ResizeMode } from "expo-av";
+import * as DocumentPicker from "expo-document-picker";
 
 import { useState } from "react";
 import { icons } from "../../constants";
+import { router } from "expo-router";
+import { createVideo } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Create = () => {
+  const { user } = useGlobalContext();
   const [uploading, setUploading] = useState(false);
   const [form, setForm] = useState({
     title: "",
@@ -17,87 +22,138 @@ const Create = () => {
     prompt: "",
   });
 
-  const submit = () => {};
+  // Function to open the image or video picker
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type:
+        selectType === "image"
+          ? ["image/png", "image/jpg", "image/jpeg"]
+          : ["video/mp4", "video/gif"],
+    });
+    
+    if (!result.canceled) {
+      if (selectType === "image") {
+        setForm({ ...form, thumbnail: result.assets[0] });
+      }
+      if (selectType === "video") {
+        setForm({ ...form, video: result.assets[0] });
+      }
+    } else {
+      setTimeout(() => {
+        Alert.alert("Document picked", JSON.stringify(result, null, 2));
+      }, 100);
+    }
+  }
 
-  return (
-    <SafeAreaView className="bg-primary h-full">
-      <ScrollView className="px-4 my-6">
-        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+    const submit = async () => {
+      if(!form.prompt || !form.title || !form.thumbnail || !form.video) {
+        return Alert.alert("Please fill in all the fields");
+      }
+      setUploading(true);
 
-        <FormField
-          title="Video Title"
-          value={form.title}
-          placeholder="Give your video a catchy title..."
-          handleChangeText={(e) => setForm({ ...form, title: e })}
-          otherStyles="mt-10"
-        />
+      try {
+        await createVideo({
+          ...form, userId: user.$id
+        });
 
-        <View className="mt-7 space-y-2">
-          <Text className="text-base text-gray-100">Upload Video</Text>
+        Alert.alert("Success", "Post uploaded successfully");
+        router.push("/home");
+      } catch (error) {
+        Alert.alert("An error occurred", error.message);
+      } finally {
+        setForm({
+          title: "",
+          thumbnail: null,
+          video: null,
+          prompt: "",
+        });
 
-          <TouchableOpacity>
-            {form.video ? (
-              <Video
-                source={{ uri: form.video.uri }}
-                className="w-full h-40 rounded-2xl"
-                useNativeControls
-                resizeMode={ResizeMode.COVER}
-                isLooping
-              />
-            ) : (
-              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
-                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+        setUploading(false);
+      }
+    };
+
+    return (
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView className="px-4 my-6">
+          <Text className="text-2xl text-white font-psemibold">
+            Upload Video
+          </Text>
+
+          <FormField
+            title="Video Title"
+            value={form.title}
+            placeholder="Give your video a catchy title..."
+            handleChangeText={(e) => setForm({ ...form, title: e })}
+            otherStyles="mt-10"
+          />
+
+          <View className="mt-7 space-y-2">
+            <Text className="text-base text-gray-100">Upload Video</Text>
+
+            <TouchableOpacity onPress={() => openPicker("video")}>
+              {form.video ? (
+                <Video
+                  source={{ uri: form.video.uri }}
+                  className="w-full h-40 rounded-2xl"
+                  resizeMode={ResizeMode.COVER}
+                />
+              ) : (
+                <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                  <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                    <Image
+                      source={icons.upload}
+                      resizeMethod="contain"
+                      className="w-1/2 h-1/2"
+                    />
+                  </View>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
+          <View className="mt-7 space-y-2">
+            <Text className="text-base text-gray-100">Thumbnail Image</Text>
+
+            <TouchableOpacity onPress={() => openPicker("image")}>
+              {form.thumbnail ? (
+                <Image
+                  source={{ uri: form.thumbnail.uri }}
+                  ResizeMode="cover"
+                  className="w-full h-64 rounded-2xl"
+                />
+              ) : (
+                <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
                   <Image
                     source={icons.upload}
                     resizeMethod="contain"
-                    className="w-1/2 h-1/2"
+                    className="w-5 h-5"
                   />
+                  <Text className="text-sm text-gray-100 font-pmedium">
+                    Choose a file
+                  </Text>
                 </View>
-              </View>
-            )}
-          </TouchableOpacity>
-        </View>
-        <View className="mt-7 space-y-2">
-          <Text className="text-base text-gray-100">Thumbnail Image</Text>
+              )}
+            </TouchableOpacity>
+          </View>
 
-          <TouchableOpacity>
-            {form.thumbnail ? (
-              <Image
-                source={{ uri: form.thumbnail.uri }}
-                ResizeMode="cover"
-                className="w-full h-64 rounded-2xl"
-              />
-            ) : (
-              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
-                <Image
-                  source={icons.upload}
-                  resizeMethod="contain"
-                  className="w-5 h-5"
-                />
-                <Text className="text-sm text-gray-100 font-pmedium">
-                  Choose a file
-                </Text>
-              </View>
-            )}
-          </TouchableOpacity>
-        </View>
+          <FormField
+            title="AI Prompt"
+            value={form.prompt}
+            placeholder="The prompt you used to create this video"
+            handleChangeText={(e) => setForm({ ...form, title: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Submit & Publish"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={uploading}
+          />
+        </ScrollView>
+      </SafeAreaView>
 
-        <FormField
-          title="AI Prompt"
-          value={form.prompt}
-          placeholder="The prompt you used to create this video"
-          handleChangeText={(e) => setForm({ ...form, title: e })}
-          otherStyles="mt-7"
-        />
-        <CustomButton
-          title="Submit & Publish"
-          handlePress={submit}
-          containerStyles="mt-7"
-          isLoading={uploading}
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+     
+    );
+  };
+
 
 export default Create;
